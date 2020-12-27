1d5c28c06b20a94a8145361f5c36822ea7427eac
AhngBeom <bbu0704@gmail.com>
2020-12-27 16:09:21 +0900

    20201227_16:09

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 15:46:58 by bahn              #+#    #+#             */
-/*   Updated: 2020/12/21 22:35:14 by bahn             ###   ########.fr       */
+/*   Updated: 2020/12/27 16:09:11 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,21 @@ char	*ft_isspace(char *str)
 
 int		ft_atoi(char *str)
 {
-	int sign;
-	int nbr;
+	long sign;
+	long nbr;
 
 	sign = 1;
 	nbr = 0;
 	str = ft_isspace(str);
-	while (*str == '-' || *str == '+')
+	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
 			sign *= -1;
 		else if (*str == '+')
 			sign *= 1;
 		str++;
-		if (!(*str >= '0' && *str <= '9') && !(*str == '-' || *str == '+'))
-			return (0);
 	}
-	while (*str >= '0' && *str <= '9')
+	while ((*str != '\0') && (*str >= '0' && *str <= '9'))
 	{
 		nbr = (nbr * 10) + (*str - 48);
 		str++;
