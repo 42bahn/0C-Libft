02160b3a2522be21ed04f2f3a0f20d4cf8d1ca03
AhngBeom <bbu0704@gmail.com>
2020-12-29 17:58:24 +0900

    20201229_17:58

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 21:22:04 by bahn              #+#    #+#             */
-/*   Updated: 2020/12/29 16:36:25 by bahn             ###   ########.fr       */
+/*   Updated: 2020/12/29 17:04:42 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 size_t	ft_countstrs(char *s, char c)
 {
-	size_t cnt;
-	
+	size_t	cnt;
+
 	cnt = 0;
 	while (*s != '\0')
 	{
-		if(ft_strchr(s, c))
+		if (ft_strchr(s, c))
 		{
 			s = ft_strchr(s, c) + 1;
 			cnt++;
@@ -33,13 +33,6 @@ size_t	ft_countstrs(char *s, char c)
 	return (cnt);
 }
 
-/*
-size_t	ft_splitlen(char *s)
-{
-	
-}
-*/
-
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
@@ -50,7 +43,6 @@ char	**ft_split(char const *s, char c)
 	sptr = (char *)s;
 	cnt_strs = ft_countstrs(sptr, c);
 	result = (char **)malloc(sizeof(char *) * cnt_strs);
-	
 	i = 0;
 	while (i < cnt_strs)
 	{
@@ -58,17 +50,14 @@ char	**ft_split(char const *s, char c)
 		{
 			result[i] = (char *)malloc(sizeof(char) * (ft_strchr(sptr, '\0') - sptr) + 1);
 			ft_strlcpy(result[i], sptr, ft_strchr(sptr, '\0') - sptr + 1);
-			printf("%s\n", result[i]);
 		}
 		else
 		{
 			result[i] = (char *)malloc(sizeof(char) * (ft_strchr(sptr, c) - sptr) + 1);
 			ft_strlcpy(result[i], sptr, ft_strchr(sptr, c) - sptr + 1);
-			printf("%s\n", result[i]);
 			sptr = ft_strchr(sptr, c) + 1;
 		}
 		i++;
 	}
-	
 	return (result);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 20:51:15 by bahn              #+#    #+#             */
-/*   Updated: 2020/12/29 14:21:25 by bahn             ###   ########.fr       */
+/*   Updated: 2020/12/29 17:00:41 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,11 @@ char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s1, char const *set);
 char		**ft_split(char const *s, char c);
+char		*ft_itoa(int n);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
 
 #endif
