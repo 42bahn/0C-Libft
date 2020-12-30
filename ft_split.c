715fccf67c5d9036ddff165048f8ef6514fbc157
AhngBeom <bbu0704@gmail.com>
2020-12-31 00:20:03 +0900

    20201231_00:19

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 21:22:04 by bahn              #+#    #+#             */
-/*   Updated: 2020/12/30 20:24:58 by bahn             ###   ########.fr       */
+/*   Updated: 2020/12/31 00:17:37 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,42 @@ size_t	ft_countstrs(char *s, char c)
 	cnt = 0;
 	while (*s != '\0')
 	{
-		if (ft_strchr(s, c))
-		{
-			s = ft_strchr(s, c) + 1;
-			cnt++;
-		}
-		else
+		if (*s++ != c)
 		{
 			cnt++;
-			break ;
+			while (*(s) != c)
+				s++;
 		}
 	}
 	return (cnt);
+}
+
+char	*ft_findstr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			s++;
+		else
+			return (s);
+	}
+	return (0);
+}
+
+size_t	ft_strclen(char *s, char c)
+{
+	size_t	length;
+	char	*ptr;
+
+	length = 0;
+	ptr = s;
+	printf("%s\n", s);
+	while (*ptr != '\0' || *ptr != c)
+	{
+		length++;
+		ptr++;
+	}
+	return (length - 1);
 }
 
 char	**ft_split(char const *s, char c)
@@ -43,9 +67,13 @@ char	**ft_split(char const *s, char c)
 	sptr = (char *)s;
 	cnt_strs = ft_countstrs(sptr, c);
 	result = (char **)malloc(sizeof(char *) * cnt_strs);
+	if (result == NULL)
+		return (0);
+	printf("%ld\n", cnt_strs);
 	i = 0;
 	while (i < cnt_strs)
 	{
+		/*
 		if (i == cnt_strs - 1)
 		{
 			result[i] = (char *)malloc((ft_strchr(sptr, '\0') - sptr) + 1);
@@ -57,6 +85,14 @@ char	**ft_split(char const *s, char c)
 			ft_strlcpy(result[i], sptr, ft_strchr(sptr, c) - sptr + 1);
 			sptr = ft_strchr(sptr, c) + 1;
 		}
+		*/
+		//result[i] = (char *)malloc(ft_strclen(ft_findstr(sptr, c), c) + 1);
+		sptr = ft_findstr(sptr, c);
+		printf("%s\n", sptr);
+		ft_strclen(sptr, c);
+		//sptr += ft_strclen(sptr, c);
+
+		
 		i++;
 	}
 	return (result);
