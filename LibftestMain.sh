b0aa1db57f73cedfeacae3f5a8733166daf01e57
Beomjun Ahn <bahn@c2r6s4.42seoul.kr>
2020-12-30 20:36:02 +0900

    20201230_20:35

--- LibftestMain.sh
+++ LibftestMain.sh
@@ -1,4 +1,4 @@
 bash ~/Libftest/grademe.sh > ~/libft/grademe
 cp ~/Libftest/deepthought ~/libft/
 #cat /dev/null > ~/libft/LibftestMain
-cat ~/Libftest/tests/Part1_functions/ft_*/main.c > ~/libft/Libftest_main
+cat ~/Libftest/tests/Part2_functions/ft_*/main.c > ~/libft/Libftest_main
