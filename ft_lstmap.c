5ffbb84321f1b6914b94586c625973217f7ebe7e
Beomjun Ahn <bahn@c2r6s4.42seoul.kr>
2021-01-01 15:58:08 +0900

    20210101_15:58

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/31 15:21:59 by bahn              #+#    #+#             */
-/*   Updated: 2021/01/01 15:23:35 by bahn             ###   ########.fr       */
+/*   Updated: 2021/01/01 15:56:13 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*lists;
-	t_list	*list;
+	size_t	i;
+	t_list	**lists;
 
 	if (!f || !del)
 		return (NULL);
-	lists = NULL;
+	lists = (t_list **)malloc(sizeof(t_list *) * (ft_lstsize(lst) + 1));
+	i = 0;
 	while (lst)
 	{
-		if (!(list = ft_lstnew(f(lst->content))))
+		if (!(lists[i] = ft_lstnew(f(lst->content))))
 		{
-			ft_lstclear(&lists, del);
+			ft_lstclear(lists, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&lists, list);
 		lst = lst->next;
+		if (i > 0)
+			lists[i - 1]->next = lists[i];
+		i++;
 	}
-	return (lists);
+	lists[i] = NULL;
+	return (*lists);
 }
