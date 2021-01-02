72f445c690391bf4728a89e1f930cb2bc7742598
AhngBeom <bbu0704@gmail.com>
2021-01-02 17:10:17 +0900

    20210102_17:10

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 16:01:24 by bahn              #+#    #+#             */
-/*   Updated: 2021/01/01 20:09:33 by bahn             ###   ########.fr       */
+/*   Updated: 2021/01/02 17:10:06 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,19 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	s1_first = (char *)s1;
 	s1_last = (char *)s1 + ft_strlen((char *)s1) - 1;
-	while (ft_strchr((char *)set, *s1_first))
+	while (ft_strchr((char *)set, *s1_first) && s1_first < s1_last)
 		s1_first++;
-	while (ft_strchr((char *)set, *s1_last))
+	while (ft_strchr((char *)set, *s1_last) && s1_last > s1)
 		s1_last--;
 	if (s1_first > s1_last)
 	{
-		result = (char *)malloc(1);
+		if (!(result = (char *)malloc(1)))
+			return (0);
 		*result = '\0';
 	}
 	else
 	{
-		result = (char *)malloc(s1_last - s1_first + 2);
-		if (!result)
+		if (!(result = (char *)malloc(s1_last - s1_first + 2)))
 			return (0);
 		ft_strlcpy(result, s1_first, s1_last - s1_first + 2);
 	}
