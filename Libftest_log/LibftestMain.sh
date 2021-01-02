e4433f74a17a61906f24d3930126b06c062b43eb
Beomjun Ahn <bahn@c2r6s4.42seoul.kr>
2021-01-02 17:27:42 +0900

    20210102_17:27

--- Libftest_log/LibftestMain.sh
+++ Libftest_log/LibftestMain.sh
@@ -1,5 +1,6 @@
-bash ~/Libftest/grademe.sh > ~/libft/grademe
-cp ~/Libftest/deepthought ~/libft/
+bash ~/Libftest/grademe.sh > ~/libft/Libftest_log/grademe
+cp ~/Libftest/deepthought ~/libft/Libftest_log/
 #cat /dev/null > ~/libft/LibftestMain
-cat ~/Libftest/tests/Part2_functions/ft_*/main.c > ~/libft/Libftest_main
-cat ~/Libftest/tests/Bonus_functions/ft_*/main.c >> ~/libft/Libftest_main
+cat ~/Libftest/tests/Part1_functions/ft_*/main.c > ~/libft/Libftest_log/Libftest_main
+cat ~/Libftest/tests/Part2_functions/ft_*/main.c >> ~/libft/Libftest_log/Libftest_main
+cat ~/Libftest/tests/Bonus_functions/ft_*/main.c >> ~/libft/Libftest_log/Libftest_main
