876f52e7d1f0459350e9407e02fa930692c94968
ahngbeom <bbu0704@gmail.com>
2022-01-20 02:26:08 +0900

    Fixed exception of ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 16:01:24 by bahn              #+#    #+#             */
-/*   Updated: 2022/01/19 00:25:59 by bahn             ###   ########.fr       */
+/*   Updated: 2022/01/20 02:24:41 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	s1_first = start_point(s1, set);
 	s1_last = last_point(s1, set);
-	if (s1_first >= s1_last)
+	if (s1_first > s1_last)
 		return (ft_strdup(""));
 	else
 	{
